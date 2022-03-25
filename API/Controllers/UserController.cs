b73fa11880fcd414418b82f07f21f658093033bb
Ana Dragicevic <anaa.dragicevic97@gmail.com>
2022-03-25 10:42:15 +0100

    Authentication, cors, HTTPS

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+         [HttpGet]
+         [AllowAnonymous]
+
+         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+             
+             return await _context.Users.ToListAsync();
+         }
+
+         [HttpGet("{id}")]
+         [Authorize]
+
+         public async Task<ActionResult<AppUser>> GetUser(int id)
+         {
+             return await _context.Users.FindAsync(id);
+         }
+    }
+
+}
